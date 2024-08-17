27b2708f211c7fab98850bebec56f99eb9aeb85f
nzejoe <jonathannzete@gmail.com>
2024-08-17 13:08:28 +0100

    navbar created

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,37 @@
+"use client";
+
+import React from "react";
+import axios from "axios";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import store from "@/common/store/reducer";
+import Navbar from "@/common/components/reuseable/Navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-    title: "Ecomarket",
-    description: "Ecomarket is a marketplace for sustainable products.",
-};
-
 export default function RootLayout({ children }) {
+    // Create a client
+    const queryClient = new QueryClient();
+
+    // set axios default baseURL
+    axios.defaults.baseURL = "https://safekart.onrender.com";
+    // axios.defaults.baseURL = "http://localhost:8000";
+    axios.defaults.withCredentials = true;
+
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <QueryClientProvider client={queryClient}>
+                    <Provider store={store}>
+                        <Navbar />
+                        {children}
+                    </Provider>
+                </QueryClientProvider>
+            </body>
         </html>
     );
 }
---
+++ common/store/reducer.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import productReducer from "./product-slice";
+
+const store = configureStore({
+    reducer: {
+        products: productReducer,
+    },
+});
+
+export default store;
