8a7344b0e6f70bf583050c3e42de49991d04ae9c
nzejoe <jonathannzete@gmail.com>
2024-08-17 14:41:02 +0100

    store page => hero section

--- app/store/page.js
+++ app/store/page.js
@@ -1,7 +1,12 @@
 import React from "react";
+import StoreBanner from "@/common/components/store/StoreBanner";
 
 const StorePage = () => {
-    return <div>StorePage</div>;
+    return (
+        <div>
+            <StoreBanner />
+        </div>
+    );
 };
 
 export default StorePage;
