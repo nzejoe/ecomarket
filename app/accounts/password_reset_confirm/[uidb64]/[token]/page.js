60d962fa7319086a158a92cca07414e6eccbbe0d
nzejoe <jonathannzete@gmail.com>
2024-08-24 16:14:32 +0100

    password reset confirm api request

--- app/accounts/password_reset_confirm/[uidb64]/[token]/page.js
+++ app/accounts/password_reset_confirm/[uidb64]/[token]/page.js
@@ -1,9 +1,13 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useRouter } from "next/navigation";
+import styles from "@/common/modules/UserPage.module.css";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 export const PassResetConfirmPage = ({ params }) => {
+    const [linkValid, setLinkValid] = useState(true);
     const router = useRouter();
 
     // Access the uidb64 and token from the URL query parameters
@@ -16,7 +20,37 @@ export const PassResetConfirmPage = ({ params }) => {
         }
     }, [uidb64, token, router]);
 
-    return <div>PassResetConfirmPage</div>;
+    const sendRequest = useCallback(async () => {
+        try {
+            const response = await axios({
+                url: `/accounts/password_reset_confirm/${uidb64}/${token}/`,
+                method: "POST",
+                headers: {
+                    "X-CSRFToken": Cookies.get("csrftoken"),
+                },
+            });
+
+            if (response.data.done) {
+                sessionStorage.setItem("user_id", response.data.user_id);
+                router.replace("/accounts/password_reset_complete/");
+            }
+        } catch (err) {
+            console.log(err);
+            setLinkValid(false);
+        }
+    }, [uidb64, token, router]);
+
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest]);
+
+    return (
+        <section className={`section ${styles.user__page}`}>
+            <div className="section__wrapper">
+                {linkValid ? <h3>please wait...</h3> : <h3 className={styles.invalid__link}>Invalid link</h3>}
+            </div>
+        </section>
+    );
 };
 
 export default PassResetConfirmPage;
