831c855e8aa19b3e70959d335359be8110b5c1ac
nzejoe <jonathannzete@gmail.com>
2024-08-24 20:42:57 +0100

    orders page

--- app/carts/order_confirmed/page.js
+++ app/carts/order_confirmed/page.js
@@ -1,12 +1,20 @@
+"use client";
+
 import React from "react";
+import Link from "next/link";
 
 const OrderConfirmedPage = () => {
     return (
         <section className={`section `}>
             <div className="section__wrapper">
                 <div>
-                    <h3>Order confirmed</h3>
-                    <p>Your order have been confirmed</p>
+                    <div className="mb-5">
+                        <h3>Order confirmed</h3>
+                        <p>Your order have been confirmed</p>
+                    </div>
+                    <Link href="/orders" className="btn btn-primary">
+                        View orders
+                    </Link>
                 </div>
             </div>
         </section>
---
+++ app/orders/page.js
@@ -0,0 +1,95 @@
+"use client";
+
+import React, { useCallback, useEffect, useState } from "react";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import LoadingSpinner from "@/common/components/reuseable/LoadingSpinner";
+
+// utils
+import { getOrderDate } from "@/common/helpers/utils";
+
+const OrdersPage = () => {
+    const { authUser } = useSelector((state) => state.users);
+    const [orders, setOrders] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getOrders = useCallback(async () => {
+        const token = authUser && authUser.token;
+        if (!token) return;
+
+        setIsLoading(true);
+
+        try {
+            const response = await axios.get("/orders/order_history/", {
+                headers: { Authorization: `token ${token}` },
+            });
+            if (response.status === 200) {
+                setOrders(response.data);
+            }
+        } catch (error) {
+            const err = { ...error };
+            console.log(err.response);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [authUser]);
+
+    useEffect(() => {
+        getOrders();
+    }, [getOrders]);
+
+    return (
+        <section className={`section `}>
+            {isLoading ? (
+                <div className="section__wrapper flex items-center justify-center h-80">
+                    <LoadingSpinner isLoading={true} color="text-primary" />
+                </div>
+            ) : (
+                <div className="section__wrapper">
+                    <h2 className="text-3xl font-semibold mb-10 capitalize">order history</h2>
+                    <div>
+                        {orders ? (
+                            <div className="overflow-x-auto">
+                                <table className="table table-zebra">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Order number</th>
+                                            <th>Date</th>
+                                            <th>Amount</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {orders.map((order) => {
+                                            return (
+                                                <tr key={order.id} className="hover">
+                                                    <td style={{ padding: "1rem" }}>{orders.indexOf(order) + 1}</td>
+                                                    <td style={{ padding: "1rem" }}>
+                                                        <Link href={`/orders/${order.order_number}`} className="link">
+                                                            {order.order_number}
+                                                        </Link>
+                                                    </td>
+                                                    <td style={{ padding: "1rem" }}>{getOrderDate(order.created)}</td>
+                                                    <td style={{ padding: "1rem" }}>
+                                                        $<span>{order.grand_total}</span>
+                                                    </td>
+                                                    <td>{order.is_ordered && "Ordered"}</td>
+                                                </tr>
+                                            );
+                                        })}
+                                    </tbody>
+                                </table>
+                            </div>
+                        ) : (
+                            <p>You have no orders yet.</p>
+                        )}
+                    </div>
+                </div>
+            )}
+        </section>
+    );
+};
+
+export default OrdersPage;
