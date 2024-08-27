0e372d3353a06bcecfb285dd617ab16aeef6dea8
nzejoe <jonathannzete@gmail.com>
2024-08-27 18:42:52 +0100

    dashboard orders

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,10 +1,13 @@
 "use client";
 
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actions as userActions } from "@/common/store/user-slice";
 import { useSales } from "@/common/context/sales-context";
 
 import TopSellingChart from "@/common/components/dashboard/TopSellingChart";
 import DashboardOrders from "@/common/components/dashboard/DashboardOrders";
+import LoadingSpinner from "@/common/components/reuseable/LoadingSpinner";
 // style
 import styles from "@/common/modules/AdminDashboard.module.css";
 // context
@@ -16,6 +19,13 @@ import { ordersCounter } from "@/common/helpers/utils";
 const DashboardPage = () => {
     const { orders, getOrders, refresh } = useSales();
 
+    const { refresh: refreshUser } = useSelector((state) => state.users);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(userActions.setUser());
+    }, [dispatch, refreshUser]);
+
     useEffect(() => {
         getOrders();
         // eslint-disable-next-line
@@ -23,26 +33,32 @@ const DashboardPage = () => {
 
     return (
         <>
-            <div className={`dashboard ${styles.main}`}>
-                <div className="max-w-md">
-                    <TopSellingChart />
+            {!orders ? (
+                <div className="h-[300px] flex items-center justify-center">
+                    <LoadingSpinner isLoading={true} color="text-primary" />
                 </div>
-                <div className={`${styles.order__cards}`}>
-                    <div className={styles.orders__card}>
-                        <h2>Total Orders</h2>
-                        <h3>{ordersCounter(orders, null)}</h3>
+            ) : (
+                <div className={`dashboard ${styles.main}`}>
+                    <div className="max-w-md">
+                        <TopSellingChart />
                     </div>
-                    <div className={`${styles.orders__card} ${styles.pending}`}>
-                        <h2>Pending Orders</h2>
-                        <h3>{ordersCounter(orders, "pending")}</h3>
+                    <div className={`${styles.order__cards}`}>
+                        <div className={styles.orders__card}>
+                            <h2>Total Orders</h2>
+                            <h3>{ordersCounter(orders, null)}</h3>
+                        </div>
+                        <div className={`${styles.orders__card} ${styles.pending}`}>
+                            <h2>Pending Orders</h2>
+                            <h3>{ordersCounter(orders, "pending")}</h3>
+                        </div>
+                        <div className={`${styles.orders__card} ${styles.delivered}`}>
+                            <h2>Delivered Orders</h2>
+                            <h3>{ordersCounter(orders, "delivered")}</h3>
+                        </div>
                     </div>
-                    <div className={`${styles.orders__card} ${styles.delivered}`}>
-                        <h2>Delivered Orders</h2>
-                        <h3>{ordersCounter(orders, "delivered")}</h3>
-                    </div>
+                    <DashboardOrders />
                 </div>
-                <DashboardOrders />
-            </div>
+            )}
         </>
     );
 };
--- common/components/dashboard/DashboardOrders.jsx
+++ common/components/dashboard/DashboardOrders.jsx
@@ -5,9 +5,81 @@ import { useSales } from "@/common/context/sales-context";
 import { getOrderDateTime } from "@/common/helpers/utils";
 // style
 import styles from "@/common/modules/DashboardOrders.module.css";
+import LoadingSkeleton from "../reuseable/LoadingSkeleton";
 
 const DashboardOrders = () => {
-    return <div>DashboardOrders</div>;
+    const { orders, refresh, getOrders, sendOrderUpdate } = useSales();
+
+    const getUpdate = (orderUpdate) => {
+        sendOrderUpdate(orderUpdate);
+    };
+
+    useEffect(() => {
+        getOrders();
+        // eslint-disable-next-line
+    }, [refresh]);
+
+    return (
+        <div className={`${styles.orders} orders`}>
+            <h2>Recent orders</h2>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Customer</th>
+                            <th>Item</th>
+                            <th>Address</th>
+                            <th>Quantity</th>
+                            <th>Amount</th>
+                            <th>Date</th>
+                            <th>Status</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders ? (
+                            orders.map((order) => {
+                                return (
+                                    <tr key={order.id}>
+                                        <td>{order.order.order_number}</td>
+                                        <td className="w-[200px]">
+                                            {order.order.first_name} {order.order.last_name}
+                                        </td>
+                                        <td className="w-[250px]">{order.product_name}</td>
+                                        <td className="min-w-[250px]">
+                                            {order.order.address_1}, {order.order.city} {order.order.state}{" "}
+                                            {order.order.country}
+                                        </td>
+                                        <td className={styles.quantity}>{order.quantity}</td>
+                                        <td>{`$${order.total_amount}`}</td>
+                                        <td className="min-w-[150px]">{getOrderDateTime(order.order.updated)}</td>
+                                        <td className={`${styles.status} ${order.status}`}>
+                                            {" "}
+                                            <span>{order.status}</span>{" "}
+                                        </td>
+                                        <td>
+                                            <div></div>
+                                        </td>
+                                    </tr>
+                                );
+                            })
+                        ) : (
+                            <>
+                                {Array.from({ length: 5 }).map((_, idx) => (
+                                    <tr key={idx}>
+                                        <td colSpan={9} className="h-[50px]">
+                                            <LoadingSkeleton />
+                                        </td>
+                                    </tr>
+                                ))}
+                            </>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default DashboardOrders;
