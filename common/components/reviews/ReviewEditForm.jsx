9d9851ab7c928a7b1e3757ac002f707a58dcc73b
nzejoe <jonathannzete@gmail.com>
2024-08-27 20:25:29 +0100

    review update

--- common/components/reviews/ReviewEditForm.jsx
+++ common/components/reviews/ReviewEditForm.jsx
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+// 3rd party
+import { useSelector } from "react-redux";
+import axios from "axios";
+import Cookies from "js-cookie";
 
-const ReviewEditForm = () => {
-    return <div>ReviewEditForm</div>;
+import RatingStar from "./RatingStar";
+// style
+import styles from "@/common/modules/Review.module.css";
+
+const ReviewEditForm = ({ userReview, handlePageRefresh, handleEdit }) => {
+    const { token } = useSelector((state) => state.users);
+    const [rating, setRating] = useState(userReview.rating);
+    const [subject, setSubject] = useState(userReview.subject);
+    const [review, setReview] = useState(userReview.review);
+    const [formHasError, setFormHasError] = useState(false);
+
+    const handleRateChange = (e) => {
+        setRating(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (rating && token) {
+            const data = {
+                product: userReview.product,
+                rating,
+                subject,
+                review,
+            };
+
+            try {
+                const response = await axios({
+                    url: `products/update_review/${userReview.id}/`,
+                    method: "PUT",
+                    headers: {
+                        "X-CSRFToken": Cookies.get("csrftoken"),
+                        Authorization: `token ${token}`,
+                    },
+                    data: data,
+                });
+
+                if (response.status === 200) {
+                    handlePageRefresh();
+                    handleEdit(false);
+                }
+            } catch (error) {
+                console.log({ ...error });
+            }
+        } else {
+            setFormHasError(true);
+        }
+    };
+
+    return (
+        <div className={styles.review__form}>
+            <h3>Edit your review</h3>
+            <form onSubmit={handleSubmit}>
+                {formHasError && <p>Please rate this product.</p>}
+                <RatingStar onChange={handleRateChange} rating={rating} />
+                <input
+                    type="text"
+                    placeholder="Enter subject"
+                    value={subject}
+                    onChange={(e) => setSubject(e.target.value)}
+                />
+                <textarea
+                    name=""
+                    id=""
+                    cols="30"
+                    rows="10"
+                    className="w-full max-w-full"
+                    placeholder="Write a short note about this product."
+                    value={review}
+                    onChange={(e) => setReview(e.target.value)}
+                ></textarea>
+                <div className={`${styles.btn__container} w-full`}>
+                    <button type="submit">Save</button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default ReviewEditForm;
